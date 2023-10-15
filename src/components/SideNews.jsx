9c441db543924d6d8f29358eeb3324e6a1b8472c
Himanshu <himanshu_196018@saitm.org>
2023-10-15 20:42:46 +0530

    added css part 2

--- src/components/SideNews.jsx
+++ src/components/SideNews.jsx
@@ -4,24 +4,31 @@ const SideNews = () => {
   return (
     <aside>
       <h2>New</h2>
-      <div className="bottom-border">
+
+      <div className="box">
         <h3>
-          <a className="hover hover-beige">Hydrogen VS Electric Cars</a>
+          <a className="hover hover-orange">Hydrogen VS Electric Cars</a>
         </h3>
         <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
       </div>
-      <div className="bottom-border">
+
+      <hr />
+
+      <div className="box">
         <h3>
-          <a className="hover hover-beige">The Downsides of AI Artistry</a>
+          <a className="hover hover-orange">The Downsides of AI Artistry</a>
         </h3>
         <p>
           What are the possible adverse effects of on-demand AI image
           generation?
         </p>
       </div>
-      <div className="bottom-border">
+
+      <hr />
+
+      <div className="box">
         <h3>
-          <a className="hover hover-beige">Is VC Funding Drying Up?</a>
+          <a className="hover hover-orange">Is VC Funding Drying Up?</a>
         </h3>
         <p>
           Private funding by VC firms is down 50% YOY. We take a look at what
