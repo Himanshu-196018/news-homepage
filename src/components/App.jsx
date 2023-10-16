19523ed4b343b4f4d2ee984d6f81e6a0c05cc0fd
Himanshu <himanshu_196018@saitm.org>
2023-10-16 19:21:18 +0530

    Redesigned web page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import Header from "./Header";
 import Top from "./Top";
 import Bottom from "./Bottom";
--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import HeroImage from "./HeroImage";
 import HeroText from "./HeroText";
 import SideNews from "./SideNews";
